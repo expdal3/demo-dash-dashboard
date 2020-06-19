d5bd4d91ccaec4d052c4e9811350f37b0bb470ec
Le <andre.le@thomsonreuters.com>
2020-06-19 10:24:06 +1000

    Initial app boilerplate 2

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import os
+import flask
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 
+server = flask.Flask(__name__)
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
 df = pd.read_csv('https://trello-attachments.s3.amazonaws.com/5bd586986a1afd42ce16e477/5ee2fa032e551158843f1905/8fb59309ca45eef878cc9c34197f4750/pop-median-rent.csv')
 
@@ -38,6 +42,9 @@ app.layout = html.Div([
         }
     )
 ])
+
+@app.callback(dash.dependencies.Output('display-value', 'children'),
+              [dash.dependencies.Input('dropdown', 'value')])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
